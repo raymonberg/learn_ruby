194e7a3f5ec96638260d36ba42279d6888fb90c1
Ray Monberg <monbergray@gmail.com>
2016-04-14 20:24:53 -0400

    Homework Update

---
+++ email.rb
@@ -0,0 +1,10 @@
+email = "ray@abc.com"
+
+if
+  email =~ (/@/)
+  puts "true"
+
+else
+  puts "false"
+
+end
--- uppercase.rb
+++ uppercase.rb
@@ -1,7 +1,7 @@
 any_case =  ["foo", "bar", "utsab"]
 
 def capitalize_method! (input)
-input.each {|word| word.upcase!} 
+input.each {|word| word.upcase!}
 end
 
 
