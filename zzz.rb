bea745710de3e91425aa9ad153795f7a2ca567bf
Ray Monberg <monbergray@gmail.com>
2016-06-23 22:48:31 -0400

    updated 6.23.16

---
+++ zzz.rb
@@ -0,0 +1,46 @@
+def has_win? (array, playerNumber)
+if board[0][0] == board[0][1] && board[0][0] == board [0][2]
+  return true
+
+
+def rows_have_win?
+
+if board[0][0] == board[0][1] && board[0][0] == board [0][2] ||
+  board[1][0] == board[1][1] && board[1][0] == board[1][2] ||
+  board[2][0] == board[2][1] && board[2][0] == board[2][2]
+  return true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+board = [['x','x','o'],['x','x','o'],['x','x','o']]
+
+
+
+
+
+
+
+
+
+board[0][0] == board[1][1] && board[0][0] == board[2][2]
+board[0][2] == board[1][1] && board[0][2] == board[2][0]
