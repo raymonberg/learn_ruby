ad8d85aa51984ed07e34a26adf685dd06e8f9141
Ray Monberg <monbergray@gmail.com>
2016-06-23 23:30:15 -0400

    tic tac toe update

--- practice.rb
+++ practice.rb
@@ -7,21 +7,3 @@ end
 odd_array = odd_method (numbers)
 
 p odd_array
-
-
-
-
-
-
-
-
-# numbers = [10, 17, 20, 37]
-
-# def odd_method (input)
-# 	input.each {|number|
-# 		if number % 2 == 1
-# 		puts number
-# 		}
-# 	end
-
-# 		odd_method (numbers)
--- zzz.rb
+++ zzz.rb
@@ -3,7 +3,7 @@ if board[0][0] == board[0][1] && board[0][0] == board [0][2]
   return true
 
 
-def rows_have_win?
+def rows_have_win? (array, playerNumber) #these are all extremely redundant.
 
 if board[0][0] == board[0][1] && board[0][0] == board [0][2] ||
   board[1][0] == board[1][1] && board[1][0] == board[1][2] ||
@@ -11,6 +11,17 @@ if board[0][0] == board[0][1] && board[0][0] == board [0][2] ||
   return true
 
 
+def cols_have_win? (array, playerNumber)
+
+  if board[0][0] == board[1][0] && board[0][0] == board [2][0] && board[0][0] != ' ' ||
+    board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[0][1] != ' ' ||
+    board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[0][1] != ' '
+    return true
+
+def diag_have_win? (array, playerNumber)
+  if board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != ' ' ||
+    board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != ' '
+    return true
 
 
 
@@ -29,10 +40,7 @@ if board[0][0] == board[0][1] && board[0][0] == board [0][2] ||
 
 
 
-
-
-
-board = [['x','x','o'],['x','x','o'],['x','x','o']]
+board = [['o','x','o'],['x','o','o'],['x','x','o']]
 
 
 
